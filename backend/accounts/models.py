297add1be5625b7466eac0b7b0ae1e5830069316
Plume_Hiira <danny900330@gmail.com>
2022-03-18 15:15:34 +0800

    setup the architecture

---
+++ backend/accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+# Create your models here.
+
+
+class User(AbstractBaseUser):
+    pass
+
+
+class Email(AbstractBaseUser):
+    pass
