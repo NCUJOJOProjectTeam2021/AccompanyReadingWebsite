0741705e0b41389a2809d5eae0a004c9335329dd
Plume_Hiira <danny900330@gmail.com>
2022-04-22 15:29:24 +0800

    delete no use things

--- TwilioBackEnd/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from api.views import RoomView, TokenView
-
-urlpatterns = [
-    path("token/<username>", TokenView.as_view(), name="token"),
-    path("rooms", RoomView.as_view(), name="room_list"),
-]
